1e96b52dae44972d720cd854d442fc0b89cff2e2
Doronin Vovka <Doronindobro@gmail.com>
2019-12-31 22:37:09 +0300

    add code

---
+++ Alina.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+print("Привет, Алинка! Введи свое любимое число.")
+very = int(input())
+textvery1 = "очень-" * very
+textvery = textvery1[0:-1] + " "
+rilke = '''Let everything happen to you,
+Beauty and terror.
+Just keep going,
+No feeling is final.
+(c) R.M.Rilke'''
+print("Я тебя " + textvery + "люблю! \nС 2020-ым годом тебя, пусть в нем будет все! <3 4-"+str(very)+"\n \n")
+print(rilke)
+int(input()) #terminal not finished
+
